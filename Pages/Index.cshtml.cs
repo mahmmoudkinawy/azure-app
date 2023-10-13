c3d561be6cd8c89c9383859201bfd852a8be779f
mahmoud.kinawy <mahmmoudkinawy@gmail.com>
2023-10-13 16:03:19 +0300

    Updated greeting in appsettings

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,14 +6,18 @@ namespace azure_app_kinawy.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
+    
+    public string Greeting { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public void OnGet()
     {
-
+        Greeting = _configuration.GetValue<string>("Greeting");
     }
 }
