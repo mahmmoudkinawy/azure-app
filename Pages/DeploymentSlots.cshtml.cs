2acd0cbcf90453bb442725058c81841508ec0428
mahmoud.kinawy <mahmmoudkinawy@gmail.com>
2023-10-15 22:10:47 +0300

    Added Changes for Staging

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_kinawy.Pages.Shared;
+public class DeploymentSlotsModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
